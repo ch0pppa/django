81056a02e9f8979b16b119cbd28a63ac4af2191c
unknown <000aleks00093@gmail.com>
2025-03-10 22:31:31 +0300

    Костяк сайта

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import context
+
+def index(request):
+    context = {
+        'title': 'HOME',
+        'content': 'Главная страница магазина - HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+    
+    return render(request, 'main/index.html', context)
+
+def  about(request):
+    return HttpResponse('About page')
+    
+    
